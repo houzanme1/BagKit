75e820132661c1d7986913657f849c98c3dd9b2b
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2010-02-08 11:08:05 +0100

    baginplace options

--- src/bag_kit/task/in_place.rb
+++ src/bag_kit/task/in_place.rb
@@ -1,7 +1,42 @@
 class BagKit
   
   class Task::InPlace < Task
+    include_package 'javax.swing'
+    include_package 'javax.swing.border'
+    include_package 'javax.swing.text'
+    include_package 'java.awt'
     include ListLayout
+    
+    def initialize(*args)
+      self.auto_close = false
+      
+      @option1 = JComboBox.new ["", "0.93", "0.94", "0.95", "0.96"].to_java(:String)
+      @option2 = JComboBox.new ["md5", "sha1", "sha256", "sha512"].to_java(:String)
+      @option3 = JComboBox.new ["md5", "sha1", "sha256", "sha512"].to_java(:String)
+      @option4 = JCheckBox.new "Retain enclosing folder"
+      super(*args) do |c, i|
+        c.option1_label = JLabel.new "Bagit version:"
+        c.option1 = @option1
+        c.option2_label = JLabel.new "Payload manifest algorithm:"
+        c.option2 = @option2
+        c.option3_label = JLabel.new "Tag manifest algorithm:"
+        c.option3 = @option3
+        c.option4 = @option4
+      end
+    end
+    
+    def layout
+      "[<toolbar         ]
+       [<option1_label   ]
+       [<option1         ]
+       [<option2_label   ]
+       [<option2         ]
+       [<option3_label   ]
+       [<option3         ]
+       [<option4         ]
+       [(500,250)*files  ]
+       [(500,250)*console]"
+    end
     
     def text_window
       "Create bags in place"
@@ -11,6 +46,10 @@ class BagKit
       "Add folders"
     end
     
+    def list_label
+      "Folders"
+    end
+    
     def before_action_add
       dialog.multi_selection_enabled = true
       dialog.file_selection_mode     = JFileChooser::DIRECTORIES_ONLY
@@ -18,7 +57,12 @@ class BagKit
     
     def action_bag(type, event)
       self.task = files.map do |folder|
-        self.class.base_task + ['baginplace', Context.local_path(folder.absolute_path)]
+        t = self.class.base_task + ['baginplace', Context.local_path(folder.absolute_path)]
+        t.push "--version",                  @option1.selected_item  if @option1.selected_index > 0
+        t.push "--payloadmanifestalgorithm", @option2.selected_item
+        t.push "--tagmanifestalgorithm",     @option3.selected_item
+        t.push "--retainbasedir" if @option4.is_selected
+        t
       end
       execute
     end
--- src/bag_kit/task/validate.rb
+++ src/bag_kit/task/validate.rb
@@ -10,22 +10,22 @@ class BagKit
     def initialize(*args)
       self.auto_close = false
       
-      @option1 = JCheckBox.new "Ignore missing bag-it.txt"
-      @option2 = JCheckBox.new "Ignore additional folders"
-      @option3 = JComboBox.new ["", "0.93", "0.94", "0.95", "0.96"].to_java(:String)
+      @option1 = JComboBox.new ["", "0.93", "0.94", "0.95", "0.96"].to_java(:String)
+      @option2 = JCheckBox.new "Ignore missing bag-it.txt"
+      @option3 = JCheckBox.new "Ignore additional folders"
       super(*args) do |c, i|
+        c.option1_label = JLabel.new "Bagit version:"
         c.option1 = @option1
         c.option2 = @option2
-        c.option3_label = JLabel.new "Bagit version:"
         c.option3 = @option3
       end
     end
     
     def layout
       "[<toolbar         ]
+       [<option1_label   ]
        [<option1         ]
        [<option2         ]
-       [<option3_label   ]
        [<option3         ]
        [(500,250)*files  ]
        [(500,250)*console]"
@@ -55,9 +55,9 @@ class BagKit
     def action_bag(type, event)
       self.task = files.map do |folder|
         t = self.class.base_task + ['verifyvalid', Context.local_path(folder.absolute_path)]
-        t.push "--version", @option3.selected_item  if @option3.selected_index > 0
-        t.push "--missingbagittolerant"             if @option1.is_selected
-        t.push "--additionaldirectorytolerant"      if @option2.is_selected
+        t.push "--version", @option1.selected_item  if @option1.selected_index > 0
+        t.push "--missingbagittolerant"             if @option2.is_selected
+        t.push "--additionaldirectorytolerant"      if @option3.is_selected
         t
       end
       execute
