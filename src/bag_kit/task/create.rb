632c43e863a18c361ca461882f4bbccc4da3d18b
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2010-02-08 11:28:08 +0100

    create options

--- src/bag_kit/task/create.rb
+++ src/bag_kit/task/create.rb
@@ -1,7 +1,44 @@
 class BagKit
   
   class Task::Create < Task
+    include_package 'javax.swing'
+    include_package 'javax.swing.border'
+    include_package 'javax.swing.text'
+    include_package 'java.awt'
     include ListLayout
+    
+    def initialize(*args)
+      self.auto_close = false
+      
+      @option1 = JComboBox.new ["", "0.93", "0.94", "0.95", "0.96"].to_java(:String)
+      @option2 = JComboBox.new ["md5", "sha1", "sha256", "sha512"].to_java(:String)
+      @option3 = JComboBox.new ["md5", "sha1", "sha256", "sha512"].to_java(:String)
+      @option4 = JComboBox.new ["filesystem", "tar", "tar_gz", "tar_bz2", "zip"].to_java(:String)
+      super(*args) do |c, i|
+        c.option1_label = JLabel.new "Bagit version:"
+        c.option1 = @option1
+        c.option2_label = JLabel.new "Payload manifest algorithm:"
+        c.option2 = @option2
+        c.option3_label = JLabel.new "Tag manifest algorithm:"
+        c.option3 = @option3
+        c.option4_label = JLabel.new "Write to:"
+        c.option4 = @option4
+      end
+    end
+    
+    def layout
+      "[<toolbar         ]
+       [<option1_label   ]
+       [<option1         ]
+       [<option2_label   ]
+       [<option2         ]
+       [<option3_label   ]
+       [<option3         ]
+       [<option4_label   ]
+       [<option4         ]
+       [(500,250)*files  ]
+       [(500,250)*console]"
+    end
     
     def text_window
       "Create a bag"
@@ -23,6 +60,10 @@ class BagKit
       end
       
       self.task = self.class.base_task + ['create', bag_path] + bag_data
+      self.task.push "--version",                  @option1.selected_item  if @option1.selected_index > 0
+      self.task.push "--payloadmanifestalgorithm", @option2.selected_item
+      self.task.push "--tagmanifestalgorithm",     @option3.selected_item
+      self.task.push "--writer",                   @option4.selected_item
       execute
     end
   end
