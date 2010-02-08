2ddbb998602fbd0617405b2c202d48b89d9e60b8
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2010-02-08 10:37:58 +0100

    update options

--- src/bag_kit/task/update.rb
+++ src/bag_kit/task/update.rb
@@ -1,7 +1,27 @@
 class BagKit
   
   class Task::Update < Task
+    include_package 'javax.swing'
+    include_package 'javax.swing.border'
+    include_package 'javax.swing.text'
+    include_package 'java.awt'
     include ListLayout
+    
+    def initialize(*args)
+      @option1 = JComboBox.new ["filesystem", "tar", "tar_gz", "tar_bz2", "zip"].to_java(:String)
+      super(*args) do |c, i|
+        c.option1_label = JLabel.new "Write to:"
+        c.option1 = @option1
+      end
+    end
+    
+    def layout
+      "[<toolbar         ]
+       [<option1_label   ]
+       [<option1         ]
+       [(500,250)*files  ]
+       [(500,250)*console]"
+    end
     
     def text_window
       "Update bags"
@@ -11,6 +31,10 @@ class BagKit
       "Add bags"
     end
     
+    def list_label
+      "Bags"
+    end
+    
     def before_action_add
       dialog.multi_selection_enabled = true
       dialog.file_selection_mode     = JFileChooser::DIRECTORIES_ONLY
@@ -18,7 +42,9 @@ class BagKit
     
     def action_bag(type, event)
       self.task = files.map do |folder|
-        self.class.base_task + ['update', Context.local_path(folder.absolute_path)]
+        self.class.base_task + [
+          'update', Context.local_path(folder.absolute_path), "--writer", @option1.selected_item
+        ]
       end
       execute
     end
--- src/bag_kit/task/validate.rb
+++ src/bag_kit/task/validate.rb
@@ -12,10 +12,11 @@ class BagKit
       
       @option1 = JCheckBox.new "Ignore missing bag-it.txt"
       @option2 = JCheckBox.new "Ignore additional folders"
-      @option3 = JComboBox.new ["version:", "0.93", "0.94", "0.95", "0.96"].to_java(:String)
+      @option3 = JComboBox.new ["", "0.93", "0.94", "0.95", "0.96"].to_java(:String)
       super(*args) do |c, i|
         c.option1 = @option1
         c.option2 = @option2
+        c.option3_label = JLabel.new "Bagit version:"
         c.option3 = @option3
       end
     end
@@ -24,6 +25,7 @@ class BagKit
       "[<toolbar         ]
        [<option1         ]
        [<option2         ]
+       [<option3_label   ]
        [<option3         ]
        [(500,250)*files  ]
        [(500,250)*console]"
@@ -52,11 +54,11 @@ class BagKit
     
     def action_bag(type, event)
       self.task = files.map do |folder|
-        options = []
-        options.push "--missingbagittolerant"             if @option1.is_selected
-        options.push "--additionaldirectorytolerant"      if @option2.is_selected
-        options.push "--version", @option3.selected_item  if @option3.selected_index > 0
-        self.class.base_task + ['verifyvalid', Context.local_path(folder.absolute_path)] + options
+        t = self.class.base_task + ['verifyvalid', Context.local_path(folder.absolute_path)]
+        t.push "--version", @option3.selected_item  if @option3.selected_index > 0
+        t.push "--missingbagittolerant"             if @option1.is_selected
+        t.push "--additionaldirectorytolerant"      if @option2.is_selected
+        t
       end
       execute
     end
